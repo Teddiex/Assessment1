ef2998496c9b06dfc18eabf2395d60331b384744
Teddiex <102158727+Teddiex@users.noreply.github.com>
2022-03-28 21:56:45 +0100

    Add files via upload
    Base code

---
+++ CMP1903M Assessment 1 Base Code/Analyse.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CMP1903M_Assessment_1_Base_Code
+{
+    public class Analyse
+    {
+        //Handles the analysis of text
+
+      
+        public List<int> analyseText(string input)
+        {
+
+            //List of integers to hold the first five measurements:
+            //1. Number of sentences
+            //2. Number of vowels
+            //3. Number of consonants
+            //4. Number of upper case letters
+            //5. Number of lower case letters
+            List<int> parameters = new List<int>();
+            //Initialise all the values in the list to '0'
+            for (int i = 0; i < 5; i++)
+            {
+                parameters.Add(0);
+            }
+
+            //Only uses the input before the *, removes other input after *
+            if (input.Contains('*'))
+            {
+                input = input.Substring(0, input.IndexOf("*"));
+            }
+
+            //count the number of sentences in the input.
+            for (int i = 0; i < input.Length; i++)
+            {
+                if (input[i] == '!' || input[i] == '.' || input[i] == '?')
+                    parameters[1]++;
+
+            }
+
+
+            //count vowels and consonants
+            for (int i = 0; i < input.Length; i++)
+            {
+                if ((input[i] >= 'a' && input[i] >= 'z') || (input[i] >= 'A' && input[i] >= 'Z'))
+                {
+                    if (input[i] == 'a' || input[i] == 'e' || input[i] == 'i' || input[i] == 'o' || input[i] == 'u')
+                        parameters[2]++; //count vowel
+
+                    else if (input[i] == 'A' || input[i] == 'E' || input[i] == 'I' || input[i] == 'O' || input[i] == 'O')
+                        parameters[2]++; //count vowel
+
+                    else parameters[3]++; //count consonant 
+                }
+
+            }
+
+            //count number of lower and upper case charecters
+            for (int i = 0; i < input.Length; i++)
+            {
+                if (input[i] == '*')
+                    break;
+                if (char.IsUpper(input[i]))
+                    parameters[0]++;
+                if (char.IsLower(input[i]))
+                    parameters[4]++;
+
+            }
+
+
+            //Create a file named longWords.txt
+            //Should be saved in CMP1903M Assessment 1 Base Code\CMP1903M Assessment 1 Base Code\bin\Debug\net6.0\longwords.txt
+            using StreamWriter file = new StreamWriter("longWords.txt");
+            string[] words = input.Split(new Char[] { ' ', '.', '!', '?', ',' }); //remove these characters
+            foreach (var w in words)
+            {
+                if (w.Trim() != " ")
+                {
+                    if (w.Length >= 7)
+                    {
+                        file.Write(w + "\n"); //write word to file + newline
+
+                    }
+                }
+
+            }
+
+            return parameters;
+        }
+    }
+}
